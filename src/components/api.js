18db5775350bbec63e0d2dbca5c52809da7d42c9
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-01-11 14:16:34 +0500

    kettt

--- src/components/api.js
+++ src/components/api.js
@@ -1,10 +1,9 @@
 export const url = 'http://192.168.76.139/'
 
-
 export const byIdObj = (id) => document.getElementById(id);
 
 export const config = {
     headers: {
-        Authorization: "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjM0NTY3ODkiLCJpYXQiOjE3MDQ5NTgwMzIsImV4cCI6MTcwNTA0NDQzMn0.k2cl1zs8fAmYbHVSwI8peZmem0Bx_wPvXFjPPpBmykopwRtWF2zc3UcCYsqKXlXsVJ2W2QlqQbtEHM22rjx3nQ",
+        Authorization: sessionStorage.getItem("jwtToken"),
     }
 }
