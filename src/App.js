0f93457183d9f322b5a0f6e5c0262617c025bfea
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-01-11 11:59:31 +0500

    ketti

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Login from './components/login';
+import User from './components/user';
+import Product from './components/product';
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </>
   )
 }
 
-export default App
+export default App;
