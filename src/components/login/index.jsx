7e4f7854961ead1f84fb1a4ee05a771d50d8c9f4
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-01-11 12:14:15 +0500

    login page style tax

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import './login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login_section">
+            <Link id="role" to=""></Link>
+            <section className="login_section-box">
+                <div className="login_box">
+                    <div className="login_content">
+                        <h2>Sign In</h2>
+                        <div className="login_form">
+                            <div className="login_inputBox" id="phoneNumber">
+                                <input id="phone_number" required /> <i>Phone Number</i>
+                            </div>
+                            <div className="login_inputBox" id="oldPassword">
+                                <input type="password" id="password" required /> <i>Password</i>
+                            </div>
+                            <div className="login_inputBox">
+                                <button className="glow-on-hover">Sign in</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
   )
 }
 
-export default Login
+export default Login;
